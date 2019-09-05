a8c157bbf8c6c227eafc43c0ce7f8437698f9e07
Julia Voronetskaja <julia.voronetskaja@tthk.ee>
2019-09-05 12:22:11 +0300

    Chuck Norris Jokes Generator

---
+++ app.js
@@ -0,0 +1,25 @@
+document.querySelector(".get-jokes").addEventListener("click", getJokes);
+
+function getJokes(e) {
+    const number = document.querySelector('input[type=number]').value;
+    
+    fetch(`http://api.icndb.com/jokes/random/${number}`)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        let output = "";
+        
+        data.value.forEach(joke => {
+            output += `<li>${joke.joke}</li>`;
+            console.log(output);
+        });
+
+        document.querySelector(".jokes").innerHTML = output;
+
+    });
+    
+    
+    e.preventDefault();
+}
+
